ebef36f435e0880049d2c0176113597b91fc4848
Chandan Kumar <ck923182@gmail.com>
2026-09-12 18:37:26 +0530

    Completed initial project setup and PostgreSQL database configuration

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -9,7 +9,7 @@ export async function testDatabaseConnection() {
     const client = await pool.connect();
     try{
         const result = await client.query("SELECT NOW()");
-        console.log("PostgreSQL connected:", result.rows[0]);
+        console.log("PostgreSQL connected :", result.rows[0]);
     }finally{
         client.release();
     }
